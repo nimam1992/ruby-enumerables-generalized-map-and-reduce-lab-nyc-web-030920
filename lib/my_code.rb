0546a5b5fe352047856d3375488d5c574012f278
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-08 17:27:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,13 +11,16 @@ def map(source_array)
   def reduce(x,y=0)
     i = 0
     statement = false
+    statement2 = false
     while i < x.length do
     if x[i] = true
       statement = true
+      statement2 = true
     else
       statement
+      statement2
     end
   i+=1
   end
-  yield statement
+  yield (statement,statement2)
 end
