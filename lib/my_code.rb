f59de7e6868dbadf52217e248d2768159f4850f1
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-08 17:12:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array << yield(source_array[i])
+    i+=1
+  end
+  new_array
+  end
+
+  def reduce(x,y=0)
+    i = 0
+    new_num = 0
+    while i < x.length do
+    new_num+=x[i]
+     i+=1
+    end
+    yield(new_num,y)
+  end
